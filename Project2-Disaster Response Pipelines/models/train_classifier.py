d185ff504e7069594ac33437c915f2e1b3054b1f
wjh2016 <524022279@qq.com>
2018-11-09 17:05:19 +0800

    feat: change the parameters of GridSeachCV

--- Project2-Disaster Response Pipelines/models/train_classifier.py
+++ Project2-Disaster Response Pipelines/models/train_classifier.py
@@ -91,13 +91,8 @@ def build_model():
     ])
     
     parameters = {
-#         'vect__ngram_range': ((1, 1), (1, 2)),
-#         'vect__max_df': (0.5, 0.75, 1.0),
         'vect__max_df': (0.5,0.75),
-#         'vect__max_features': (None, 5000, 10000),
-#         'tfidf__use_idf': (True, False),
-        'clf__estimator__n_estimators': [50, 100, 200],
-        'clf__estimator__min_samples_split': [2, 3, 4]
+        'clf__estimator__n_estimators': [25,50],
     }
 
     cv = GridSearchCV(pipeline,param_grid=parameters,n_jobs=-1,verbose=2)
